97c650c53e4d7d446116c8cbdd12d93576762468
jack-adorbs <131496393+jack-adorbs@users.noreply.github.com>
2023-06-22 12:33:11 +0100

    Add files via upload

---
+++ clean_name.rkt
@@ -0,0 +1,18 @@
+#! /usr/bin/env racket
+#lang racket
+
+(define (no-space str)
+  (string-join (string-split (string-trim (string-normalize-spaces str)) " ") "_"))
+
+(define (dot-file? path)
+  (char=? (string-ref (path->string path) 0) #\.))
+
+(define paths-to-change
+  (filter (λ (x) (not (string=? (no-space (path->string x)) (path->string x))))
+          (filter (λ (x) (not (dot-file? x))) (directory-list))))
+
+(define (clean-names)
+  (for ([i paths-to-change])
+    (rename-file-or-directory i (no-space (path->string i)))))
+
+(clean-names)
